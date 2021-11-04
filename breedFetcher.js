44cdafcfaaf41bffce42d7398d3030950563c3f8
vagrant <arlo@gmail.com>
2021-11-04 20:46:24 +0000

    This is the next phase

--- breedFetcher.js
+++ breedFetcher.js
@@ -9,9 +9,6 @@ const cat = gnort[0];
 //const words = ""
 
 request('https://api.thecatapi.com/v1/breeds/search?q=' + cat, (error, response, body) => {
-  
-
-
   //console.log('error:', error); // Print the error if one occurred
   // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   // console.log('body:', body); // Print the HTML for the Google homepage.
